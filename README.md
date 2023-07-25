37afa0adea534ab7ef8d916f435e9088064b6bd2
Abbas Al Khalifa <info@devf5r.com>
2023-07-25 08:47:10 +0300

    0

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # sshlogin
- sshlogin
+apt update && apt install wget -y && wget https://raw.githubusercontent.com/Andyyuda/sshlogin/main/telegram_ssh_login_alert.sh && chmod +x telegram_ssh_login_alert.sh && ./telegram_ssh_login_alert.sh
---
+++ telegram_ssh_login_alert.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+#https://github.com/indonesia08
+
+# Data user id dan token bot telegram yang kamu dapatkan dari @BotFather on Telegram
+USERID="DRVf5r_net_bot"
+KEY="6034609193:AAGNok_JI8idIyxOsmxOHIpyrVRbq8_PY4U"
+
+URL="https://api.telegram.org/bot${KEY}/sendMessage"
+DATE="$(date "+%d %b %Y %H:%M")"
+
+if [ -n "$SSH_CLIENT" ]; then
+        CLIENT_IP=$(echo $SSH_CLIENT | awk '{print $1}')
+
+        SRV_HOSTNAME=$(hostname -f)
+        SRV_IP=$(hostname -I | awk '{print $1}')
+
+IPINFO="https://ipinfo.io/${CLIENT_IP}"
+
+TEXT="SSH login
+================
+
+*${SRV_HOSTNAME}*
+Use ${USER}@*${SRV_IP}*
+From *${CLIENT_IP}*
+Date: ${DATE}
+[${IPINFO}](${IPINFO})"
+
+curl -s -d "chat_id=${USERID}&text=${TEXT}&disable_web_page_preview=true&parse_mode=markdown" $URL > /dev/null
+fi
